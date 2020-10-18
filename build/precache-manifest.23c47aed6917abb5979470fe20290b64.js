b8c96a35fb129f7872531edd9e6524186aa70f92
smitptl <smitvptl@gmail.com>
2020-10-18 15:12:12 -0700

    add chat msg and channel name functionality to the iMessage

---
+++ build/precache-manifest.23c47aed6917abb5979470fe20290b64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a6d9e452930b207bf3f67c5e05c076",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e87acde2fe28c333003b",
+    "url": "/static/css/main.a576f6ea.chunk.css"
+  },
+  {
+    "revision": "fcd72dfb1b80d5c9e922",
+    "url": "/static/js/2.34fa930f.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/static/js/2.34fa930f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e87acde2fe28c333003b",
+    "url": "/static/js/main.ee4f1d52.chunk.js"
+  },
+  {
+    "revision": "ed01609efa8e8d34ac5f",
+    "url": "/static/js/runtime-main.4376f7a4.js"
+  }
+]);
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,15 +1,37 @@
 import { Avatar, IconButton } from "@material-ui/core";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Sidebar.css";
 import SearchIcon from "@material-ui/icons/Search";
 import RateReviewOutlinedIcon from "@material-ui/icons/RateReviewOutlined";
 import SidebarChat from "./SidebarChat";
 import { useSelector } from "react-redux";
 import { selectUser } from "./features/userSlice";
-import { auth } from "./firebase";
+import db, { auth } from "./firebase";
 
 function Sidebar() {
   const user = useSelector(selectUser);
+  const [chats, setChats] = useState([]);
+
+  useEffect(() => {
+    db.collection("chats").onSnapshot((snapshot) =>
+      setChats(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      )
+    );
+  }, []);
+
+  const addChat = () => {
+    const chatName = prompt("Please enter a chat name");
+
+    if (chatName) {
+      db.collection("chats").add({
+        chatName: chatName,
+      });
+    }
+  };
   return (
     <div className="sidebar">
       <div className="sidebar__header">
@@ -23,13 +45,13 @@ function Sidebar() {
           <input placeholder="Search" />
         </div>
         <IconButton variant="outlined" className="sidebar__inputButton">
-          <RateReviewOutlinedIcon />
+          <RateReviewOutlinedIcon onClick={addChat} />
         </IconButton>
       </div>
       <div className="sidebar__chats">
-        <SidebarChat />
-        <SidebarChat />
-        <SidebarChat />
+        {chats.map(({ id, data: { chatName } }) => (
+          <SidebarChat key={id} id={id} chatName={chatName} />
+        ))}
       </div>
     </div>
   );
